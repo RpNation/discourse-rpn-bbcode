4a0a1f0488580da48d9adb6a732625aca56fd8a8
unknown <Ghan@GHAN-DT02.ghannet.com>
2020-05-02 18:25:56 -0500

    Adding plugin file

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# name: discourse-rpn-bbcode
+# about: Custom BBCode for RpNation
+# version: 0.1
+# authors: Ghan
+# url: https://github.com/Ghan04/discourse-rpn-bbcode

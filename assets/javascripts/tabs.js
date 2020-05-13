9d4c793e2a94fea25d531ffee0cd67873aacd55a
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-05-13 14:45:18 -0700

    Update Tabs to not use id

--- assets/javascripts/tabs.js
+++ assets/javascripts/tabs.js
@@ -1,13 +1,15 @@
-function openRPNTab(evt, RPNTabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("rpntabcontent");
+function openRPNTab(evt) {
+  let i, tabcontent, tablinks;
+  let parent = evt.currentTarget.parentElement;
+
+  tabcontent = parent.getElementsByClassName("rpntabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("rpntablinks");
+  tablinks = parent.getElementsByClassName("rpntablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(RPNTabName).style.display = "block";
+  evt.currentTarget.nextElementSibling.style.display = "block";
   evt.currentTarget.className += " active";
 }

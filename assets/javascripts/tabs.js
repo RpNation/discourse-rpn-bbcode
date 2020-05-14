94e6784dcdde882326361fe2c7ffabee4bc8a1a6
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-05-13 18:27:53 -0700

    Tab Fix

--- assets/javascripts/tabs.js
+++ assets/javascripts/tabs.js
@@ -1,13 +1,12 @@
 function openRPNTab(evt) {
-  let i, tabcontent, tablinks;
   let parent = evt.currentTarget.parentElement;
 
-  tabcontent = parent.getElementsByClassName("rpntabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  var tabcontent = parent.getElementsByClassName("rpntabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = parent.getElementsByClassName("rpntablinks");
-  for (i = 0; i < tablinks.length; i++) {
+  var tablinks = parent.getElementsByClassName("rpntablinks");
+  for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   evt.currentTarget.nextElementSibling.style.display = "block";

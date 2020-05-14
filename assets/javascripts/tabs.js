d00e24d1f7ccbb673e02ff39bac0f21da47433f1
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-05-13 18:44:15 -0700

    Update tabs.js

--- assets/javascripts/tabs.js
+++ assets/javascripts/tabs.js
@@ -1,12 +1,12 @@
 function openRPNTab(evt) {
-  let parent = evt.currentTarget.parentElement;
+  var parent = evt.currentTarget.parentElement;
 
   var tabcontent = parent.getElementsByClassName("rpntabcontent");
-  for (let i = 0; i < tabcontent.length; i++) {
+  for (var i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   var tablinks = parent.getElementsByClassName("rpntablinks");
-  for (let i = 0; i < tablinks.length; i++) {
+  for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   evt.currentTarget.nextElementSibling.style.display = "block";

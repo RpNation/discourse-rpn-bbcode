b6bdbb6b15da3f2e6188fa249fc6f4cf185f7715
Mary <mshultz@student.neumont.edu>
2020-05-15 01:58:59 -0600

    removes asset import

--- plugin.rb
+++ plugin.rb
@@ -6,5 +6,4 @@
 # authors: Ghan
 # url: https://github.com/Ghan04/discourse-rpn-bbcode
 register_asset "javascripts/tabs.js"
-register_asset "javascripts/fonts.js"
 register_asset "stylesheets/bbcode.scss"

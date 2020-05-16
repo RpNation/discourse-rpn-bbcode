c53757f12900ed3471605a228fa98f3e44cbbc90
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-05-15 22:55:18 -0700

    Rename tab function

--- assets/javascripts/tabs.js
+++ assets/javascripts/tabs.js
@@ -1,11 +1,11 @@
-function openRPNTab(evt) {
+function openBBCodeTab(evt) {
   var parent = evt.currentTarget.parentElement;
 
-  var tabcontent = parent.getElementsByClassName("rpntabcontent");
+  var tabcontent = parent.getElementsByClassName("bbcode-tab-content");
   for (var i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  var tablinks = parent.getElementsByClassName("rpntablinks");
+  var tablinks = parent.getElementsByClassName("bbcode-tab-links");
   for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }

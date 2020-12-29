f5f2c19e55fe2f18b5cc1689a24d6c13cf2dcd08
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-12-29 09:32:18 -0800

    prettier styling

--- assets/javascripts/spoiler.js
+++ assets/javascripts/spoiler.js
@@ -1,69 +1,67 @@
 function toggleBBCodeSpoiler(evt) {
-
-  function slideUp (target, duration) {
-  	target.style.transitionProperty = 'height, margin, padding';
-  	target.style.transitionDuration = duration + 'ms';
-  	target.style.boxSizing = 'border-box';
-  	target.style.height = target.offsetHeight + 'px';
-  	target.offsetHeight;
-  	target.style.overflow = 'hidden';
-  	target.style.height = 0;
-  	target.style.paddingTop = 0;
-  	target.style.paddingBottom = 0;
-  	target.style.marginTop = 0;
-  	target.style.marginBottom = 0;
-  	window.setTimeout( function() {
-  	  target.style.display = 'none';
-  	  target.style.removeProperty('height');
-  	  target.style.removeProperty('padding-top');
-  	  target.style.removeProperty('padding-bottom');
-  	  target.style.removeProperty('margin-top');
-  	  target.style.removeProperty('margin-bottom');
-  	  target.style.removeProperty('overflow');
-  	  target.style.removeProperty('transition-duration');
-  	  target.style.removeProperty('transition-property');
-  	}, duration);
+  function slideUp(target, duration) {
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.boxSizing = "border-box";
+    target.style.height = target.offsetHeight + "px";
+    target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(function () {
+      target.style.display = "none";
+      target.style.removeProperty("height");
+      target.style.removeProperty("padding-top");
+      target.style.removeProperty("padding-bottom");
+      target.style.removeProperty("margin-top");
+      target.style.removeProperty("margin-bottom");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+    }, duration);
   }
 
-  function slideDown (target, duration) {
-    target.style.removeProperty('display');
+  function slideDown(target, duration) {
+    target.style.removeProperty("display");
     var display = window.getComputedStyle(target).display;
 
-    if (display === 'none')
-      display = 'block';
+    if (display === "none") display = "block";
 
     target.style.display = display;
     var height = target.offsetHeight;
-    target.style.overflow = 'hidden';
+    target.style.overflow = "hidden";
     target.style.height = 0;
     target.style.paddingTop = 0;
     target.style.paddingBottom = 0;
     target.style.marginTop = 0;
     target.style.marginBottom = 0;
     target.offsetHeight;
-    target.style.boxSizing = 'border-box';
+    target.style.boxSizing = "border-box";
     target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + 'ms';
-    target.style.height = height + 'px';
-    target.style.removeProperty('padding-top');
-    target.style.removeProperty('padding-bottom');
-    target.style.removeProperty('margin-top');
-    target.style.removeProperty('margin-bottom');
-    window.setTimeout( function() {
-      target.style.removeProperty('height');
-      target.style.removeProperty('overflow');
-      target.style.removeProperty('transition-duration');
-      target.style.removeProperty('transition-property');
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = height + "px";
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    window.setTimeout(function () {
+      target.style.removeProperty("height");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
     }, duration);
   }
 
   function slideToggle(target, duration) {
-	if (window.getComputedStyle(target).display === 'none') {
-	  return slideDown(target, duration);
-	} else {
-	  return slideUp(target, duration);
-	}
+    if (window.getComputedStyle(target).display === "none") {
+      return slideDown(target, duration);
+    } else {
+      return slideUp(target, duration);
+    }
   }
   var slide = evt.currentTarget.nextElementSibling;
-  slideToggle(slide,500);
+  slideToggle(slide, 500);
 }

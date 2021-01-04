68d498507fa1179d119a51cc59dfe24c533790cf
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2021-01-04 13:33:23 -0800

    add lazy loading to accordions

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,4 @@
 # authors: Ghan, Alteras, TheDarkWizard, FaithWynters
 # url: https://github.com/RpNation/discourse-rpn-bbcode
 register_asset "javascripts/tabs.js"
-register_asset "javascripts/accordion.js"
-
 register_asset "stylesheets/bbcode.scss"
